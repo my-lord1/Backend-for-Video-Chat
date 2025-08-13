b884ac813d53c358003b655f961e30fa266bffc6
Nivas Reddy <bn123@snu.edu.in>
2025-08-13 21:09:09 +0530

    ,,

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const socketIdToRoom = {};  // socket.id => roomId
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: "https://videochat-two.vercel.app/", 
     methods: ["GET", "POST"],
     credentials: true
   }));
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173", 
+      origin: "https://videochat-two.vercel.app/", 
       methods: ["GET", "POST"],
       credentials: true
     }
